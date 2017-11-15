aac30e953f2e229eb1aedd37a80403b31d91bbe5
BReily89 <BrandonReily.br@gmail.com>
2017-11-15 10:45:17 -0500

    component will mount in posts

--- client/src/components/Posts.js
+++ client/src/components/Posts.js
@@ -1,6 +1,24 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 
 class Posts extends Component {
+    state ={
+        posts: {},
+        redirectToJournals: false
+    }
+    async componentWillMount() {
+        try{
+            const journalId = this.props.match.params.journalId
+            const postsId = this.props.match.params.postsId
+
+            const response = await axios.get(`/api/journals${journalId}/posts/${postsId}`)
+            console.table(response.data)
+            this.setState({post: response.data})
+        }catch (error){
+            console.log(error)
+        }
+    console.log(this.prope)
+    }
 
     render() {
         return (
