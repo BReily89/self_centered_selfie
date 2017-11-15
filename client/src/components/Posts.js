0003c2999a7fb0b553d8727b96cc033e8e5dff9c
BReily89 <BrandonReily.br@gmail.com>
2017-11-15 10:34:52 -0500

    writing in wrong file

--- client/src/components/Posts.js
+++ client/src/components/Posts.js
@@ -1,20 +1,10 @@
 import React, { Component } from 'react';
-import timeage from 'timeago'
 
 class Posts extends Component {
 
     render() {
-        const {posts} = this.props
         return (
             <div>
-                {
-                    posts.map((post) => {
-                        const content = post.content.splice(" ")(.0, 50).join(" ")
-                        const timeAgo = timeAgo().format(post.created_at)
-
-                    })
-
-                }
                 
             </div>
         );
